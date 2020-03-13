92e5c6ef7c924912470c6772c0b4857e8edc337d
Christophe Navarro <christophe.navarro@epitech.eu>
2020-03-13 09:32:23 +0100

    first push

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Puissance4</title>
+  <meta charset="UTF-8">
+</head> 
+<body>     
+<script src="public/script/jquery-3.4.1.min.js"></script>
+<div id="result"></div>
+<script src="main.js"></script>
+<script>
+$('body').puissance4({
+    // color1: 'pink',
+    // color2: 'blue',
+    // nbx: '10',
+    // nby: '10'
+  });  
+</script>
+</body>
+</html>
